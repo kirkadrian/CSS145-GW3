8ad7eaabce7854d4c98dfed920d29525ee1bd2d2
kirk928 <kirk.sunico@gmail.com>
2024-10-07 21:50:08 +0800

    changing dataset path test

--- Group.py
+++ Group.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 st.title("GROUP 4")
 st.header("Laptop Prices Dataset")
 
-df = pd.read_csv("dataset/laptop_price.csv")
+df = pd.read_csv("laptop_price.csv")
 
 #Manalang 1
 
