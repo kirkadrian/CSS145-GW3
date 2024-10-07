b6b88f515263f6ba5a1737f690ffbbdecced5f4b
kirk928 <kirk.sunico@gmail.com>
2024-10-07 21:56:31 +0800

    test

--- Group.py
+++ Group.py
@@ -26,19 +26,19 @@ st.pyplot(plt)
 
 #Manalang 2
 
-df = pd.read_csv('laptop_price.csv')
-custom_order = ['Notebook', 'Gaming', 'Ultrabook', 'Netbook', '2 in 1 Convertible', 'Workstation']
+#df = pd.read_csv('laptop_price.csv')
+#custom_order = ['Notebook', 'Gaming', 'Ultrabook', 'Netbook', '2 in 1 Convertible', 'Workstation']
 
-data = df['laptop_type'].value_counts() 
-data = data.reindex(custom_order).fillna(0)
-color = ['#7AD9F8', '#A8D600', '#F15A5E', '#D46783', '#FFB84D', '#B47FC1']
-explode = [0.05] * len(data)
+#data = df['laptop_type'].value_counts() 
+#data = data.reindex(custom_order).fillna(0)
+#color = ['#7AD9F8', '#A8D600', '#F15A5E', '#D46783', '#FFB84D', '#B47FC1']
+#explode = [0.05] * len(data)
 
-plt.figure(figsize=(10, 8))
-plt.pie(data, labels=data.index, autopct='%1.1f%%', startangle=40, colors=color, explode=explode, shadow=True, wedgeprops={'edgecolor': 'black', 'linewidth': 0.5, 'antialiased': True})
-plt.title('Distribution of Laptop Types', fontsize=12, fontweight='bold')
-plt.axis('equal')
-plt.show()
+#plt.figure(figsize=(10, 8))
+#plt.pie(data, labels=data.index, autopct='%1.1f%%', startangle=40, colors=color, explode=explode, shadow=True, wedgeprops={'edgecolor': 'black', 'linewidth': 0.5, 'antialiased': True})
+#plt.title('Distribution of Laptop Types', fontsize=12, fontweight='bold')
+#plt.axis('equal')
+#plt.show()
 
 
 #Sunico 1
